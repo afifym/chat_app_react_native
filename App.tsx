cd9f90d3a2525b950b3679fba2e55b147b3c8de6
afifym <mohamed.afify323@gmail.com>
2021-06-05 14:34:14 +0200

    added aws graphql api

--- App.tsx
+++ App.tsx
@@ -1,19 +1,70 @@
 import 'react-native-gesture-handler';
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
+import React, { useEffect } from 'react';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
 
 import useCachedResources from './hooks/useCachedResources';
 import useColorScheme from './hooks/useColorScheme';
 import Navigation from './navigation';
 
+// @ts-ignore
+import { withAuthenticator } from 'aws-amplify-react-native';
+
+import { API, Auth, graphqlOperation } from 'aws-amplify';
+
 import Amplify from 'aws-amplify';
 import config from './src/aws-exports';
+import { getUser } from './src/graphql/queries';
+import { createUser } from './src/graphql/mutations';
 Amplify.configure(config);
 
-export default function App() {
+const randomImages = [
+  'https://hieumobile.com/wp-content/uploads/avatar-among-us-2.jpg',
+  'https://hieumobile.com/wp-content/uploads/avatar-among-us-3.jpg',
+  'https://hieumobile.com/wp-content/uploads/avatar-among-us-6.jpg',
+  'https://hieumobile.com/wp-content/uploads/avatar-among-us-9.jpg',
+];
+
+function App() {
   const isLoadingComplete = useCachedResources();
   const colorScheme = useColorScheme();
+
+  const getRandomImage = () => {
+    return randomImages[Math.floor(Math.random() * randomImages.length)];
+  };
+
+  useEffect(() => {
+    const fetchUser = async () => {
+      // get authenticated user from auth
+      const userInfo = await Auth.currentAuthenticatedUser({
+        bypassCache: true,
+      });
+
+      if (userInfo) {
+        const userData = await API.graphql(
+          graphqlOperation(getUser, { id: userInfo.attributes.sub })
+        );
+
+        if (userData?.data?.getUser) {
+          console.log('user is already registered in database');
+          return;
+        }
+
+        const newUser = {
+          id: userInfo.attributes.sub,
+          name: userInfo.username,
+          imageUri: getRandomImage(),
+          status: 'Hey there!',
+        };
+
+        await API.graphql(graphqlOperation(createUser, { input: newUser }));
+      }
+
+      // get user data from backend
+    };
+
+    fetchUser();
+  }, []);
 
   if (!isLoadingComplete) {
     return null;
@@ -26,3 +77,5 @@ export default function App() {
     );
   }
 }
+
+export default withAuthenticator(App);
